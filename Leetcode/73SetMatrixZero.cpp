b706f60f56c17f3c436f3e8c4c817c157aa7916b
Abhijeet Gupta <44775514+abhijeet010304@users.noreply.github.com>
2021-01-04 15:02:13 +0530

    D2

--- Leetcode/73SetMatrixZero.cpp
+++ Leetcode/73SetMatrixZero.cpp
@@ -66,3 +66,68 @@ public:
     }
 };
 */
+
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int row = matrix.size(),col = matrix[0].size(),i,j;
+        if(row==1){
+            bool found =false;
+            for(i=0;i<col;i++){
+                if(matrix[0][i]==0)found =true;
+            }
+            if(found){
+                for(i=0;i<col;i++){
+                    matrix[0][i]=0;
+                }
+            }
+        }
+
+        else if(col==1){
+            bool found =false;
+            for(i=0;i<row;i++){
+                if(matrix[i][0]==0)found =true;
+            }
+            if(found){
+                for(i=0;i<row;i++){
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        else{
+            bool found1 = false,found2 = false;
+            for(i=0;i<col;i++){
+                 if(  matrix[0][i]==0) found1=true;
+            }
+            for(i=0;i<row;i++){
+                 if(  matrix[i][0]==0) found2=true;
+            }
+
+            for(i=1;i<row;i++){
+                for(j=1;j<col;j++){
+                    if(matrix[i][j]==0){
+                        matrix[i][0]=0;
+                        matrix[0][j]=0;
+                    }
+
+                }
+            }
+
+            for(i=row-1;i>=0;i--){
+                for(j=col-1;j>=0;j--){
+                    if(matrix[i][0]==0 or matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+            if(found1){
+                for(i=0;i<col;i++)matrix[0][i]=0;
+            }
+             if(found2){
+                for(i=0;i<row;i++)matrix[i][0]=0;
+            }
+        }
+    }
+};
