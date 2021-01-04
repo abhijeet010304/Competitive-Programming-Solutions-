3bc62b99c5ddb324cfcf6a569fb5eb97e70119ac
Abhijeet Gupta <44775514+abhijeet010304@users.noreply.github.com>
2021-01-04 12:47:04 +0530

    D2

--- Leetcode/73SetMatrixZero.cpp
+++ Leetcode/73SetMatrixZero.cpp
@@ -28,3 +28,41 @@ public:
     }
 };
 */
+
+/*
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int row = matrix.size(),col = matrix[0].size(),i,j;
+        vector<int> r1,c1;
+        for(i=0;i<row;i++){
+            for(j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    r1.push_back(i);
+                    c1.push_back(j);
+
+                }
+            }
+        }
+        int n = r1.size();
+        for(int l=0;l<n;l++){
+           int i = r1[l];
+           int j = c1[l];
+             for(int k = 0;k<col;k++){
+                matrix[i][k]=0;
+
+            }
+
+            for(int k = 0;k<row;k++){
+                matrix[k][j]=0;
+
+            }
+
+        }
+
+
+
+
+    }
+};
+*/
