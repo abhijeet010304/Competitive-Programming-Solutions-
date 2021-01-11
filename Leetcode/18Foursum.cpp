0b50fe6182b5dcf750f826f605ab7a5cffa348f4
Abhijeet Gupta <44775514+abhijeet010304@users.noreply.github.com>
2021-01-11 22:06:58 +0530

    D4

--- Leetcode/18Foursum.cpp
+++ Leetcode/18Foursum.cpp
@@ -1,4 +1,5 @@
 /* Pure Brute force approach*/
+/*
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
@@ -33,5 +34,85 @@ public:
 
     }
 
-    
+
+};
+*/
+
+/*
+optimized with 2pointer time = O(n^3)
+
+
+*/
+class Solution {
+public:
+    vector<int> global;
+    int n;
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int i=0, j;
+        n = nums.size();
+        sort(nums.begin(),nums.end());
+        global = nums;
+        vector<vector<int>> ret;
+
+        while(i<n){
+            j = i+1;
+            while(j<n){
+                int l = j+1,r = n-1;
+                while(l<r){
+                    int current = nums[i]+nums[j];
+                    int need = target - current;
+                    vector<int>ans = search(l,r,need);
+                    if(ans.size()==2){
+                        ans.push_back(nums[i]);
+                        ans.push_back(nums[j]);
+                        ret.push_back(ans);
+                    }
+                    skip(l,r);
+                }
+                skip(j);
+            }
+
+            skip(i);
+
+        }
+        return ret;
+    }
+
+    vector<int> search(int &l,int &r, int target){
+        vector<int> ans;
+        if(l>=r) return ans;
+        while(l<r){
+                if(global[l]+global[r]==target){
+                ans.push_back(global[l]);
+                ans.push_back(global[r]);
+                return ans;
+            }
+            else{
+                if(global[l]+global[r]>(target))r--;
+                else l++;
+
+                if(target==1)cout<<l<<" ";
+            }
+        }
+        return ans ;
+    }
+    void skip(int &l, int &r){
+        if(l>=r) return ;
+        int prev = global[l];
+
+        if(l<n-1)l++;
+        while(l<n and global[l]==prev)l++;
+        prev = global[r];
+        if(r>=l+1)r--;
+        while(r>l and global[r]==prev )r--;
+    }
+     void skip(int &l){
+
+        if(l>=n) return ;
+        int prev = global[l];
+        if(l<n-1)l++;
+        while(l<n and global[l]==prev)l++;
+
+    }
+
 };
