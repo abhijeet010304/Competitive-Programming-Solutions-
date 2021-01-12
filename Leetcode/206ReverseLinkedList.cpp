9935fc6b72457b939f1cf3343553960389708967
Abhijeet Gupta <44775514+abhijeet010304@users.noreply.github.com>
2021-01-13 00:07:31 +0530

    D5

--- Leetcode/206ReverseLinkedList.cpp
+++ Leetcode/206ReverseLinkedList.cpp
@@ -36,3 +36,46 @@ public:
          return temp2;
      }
 };
+
+
+/*
+Iterative
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL or !head->next)return head;
+        if(!head->next->next){
+            ListNode* temp = head->next;
+            head->next=NULL;
+            temp->next = head;
+            return temp;
+        }
+
+            ListNode* prev = NULL,*curr = head, *Next1 = curr->next,*Next2 = Next1->next;
+            while(Next2){
+                ListNode* temp = curr;
+                curr ->next = prev;
+
+                prev = curr;
+                curr = Next1;
+                Next1 = Next2;
+                Next2=Next2->next;
+            }
+        curr->next = prev;
+        Next1->next = curr;
+
+        return Next1;
+    }
+};
