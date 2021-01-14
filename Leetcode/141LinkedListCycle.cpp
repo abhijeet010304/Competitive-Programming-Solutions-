a34f924a90cbd1e84abe778fc098e3cf74243e63
Abhijeet Gupta <44775514+abhijeet010304@users.noreply.github.com>
2021-01-14 19:03:45 +0530

    D6

---
+++ Leetcode/141LinkedListCycle.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow= head,*fast = head;
+        while(true){
+            if(!slow or !fast){
+                return false;
+
+            }
+
+            slow = slow->next;
+            fast = fast->next;
+            if(!fast)return false;
+            fast = fast->next;
+
+            if(slow==fast)return true;
+
+        }
+        return false;
+
+    }
+};
