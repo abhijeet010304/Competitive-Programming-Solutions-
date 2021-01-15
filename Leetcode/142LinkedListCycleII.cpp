fac104894db8b955a17d0b4a461eb31fce5b448d
Abhijeet Gupta <44775514+abhijeet010304@users.noreply.github.com>
2021-01-16 01:52:15 +0530

    D6

--- Leetcode/142LinkedListCycleII.cpp
+++ Leetcode/142LinkedListCycleII.cpp
@@ -25,3 +25,67 @@ public:
         return temp;
     }
 };
+
+/*
+TC: O(n^2)
+SC: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head)return head;
+        if(head->next==NULL)return NULL;
+        else if(head->next==head)return head;
+
+        ListNode*  temp = head,*slow =head,*fast = head;
+        do{
+            slow = slow->next;
+            fast = fast->next;
+            if(fast)fast = fast->next;
+            else break;
+        }while(slow and fast and slow!=fast);
+
+        if(slow!=fast)return NULL;
+        else {
+            int ans = 1e9;
+            ListNode *node =NULL;
+            int count =0;
+            while(temp!=slow){
+                count ++;
+                temp = temp->next;
+            }
+            if(count<ans){
+                ans =count;
+                node = slow;
+            }
+
+            slow = slow->next;
+            while(slow!=fast){
+                temp = head;
+                count =0;
+                while(temp!=slow){
+                    count ++;
+                    temp = temp->next;
+                }
+                if(count<ans){
+                    ans =count;
+                    node = slow;
+                }
+                slow = slow->next;
+            }
+
+
+            return node;
+        }
+
+    }
+};
