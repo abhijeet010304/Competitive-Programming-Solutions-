6f9ddd0abc76183c3cb9d48d487067181ef22c8c
Abhijeet Gupta <44775514+abhijeet010304@users.noreply.github.com>
2021-01-18 22:43:49 +0530

    D7

--- Leetcode/26RemoveDuplicatesfromSortedArray.cpp
+++ Leetcode/26RemoveDuplicatesfromSortedArray.cpp
@@ -22,3 +22,32 @@ public:
         return n;
     }
 };
+
+
+/*
+Approach 2
+*/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int n= nums.size();
+        if(n<2)return n;
+        int p1 = 0,p2 =1,ind = 0,val = nums[p1];
+        while(p2<n){
+
+
+            while(p2<n and nums[p2]==val){
+                p2++;
+            }
+            p1++;
+            if(p2<n)
+                nums[p1] = nums[p2];
+            val = nums[p1];
+
+        }
+
+
+        return p1;
+    }
+};
