911f9d9dc895bd8300a6891dc17d304f4e8425c5
Abhijeet Gupta <44775514+abhijeet010304@users.noreply.github.com>
2021-02-01 21:27:32 +0530

    D14

--- Leetcode/191Numberof1Bits.cpp
+++ Leetcode/191Numberof1Bits.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-
+        int count =0;
+        string a = bitset<32>(n).to_string();
+        // cout<<a<<"\n";
+        for(auto c:a){
+            if(c=='1')count++;
+        }
+        return count ;
     }
 };
